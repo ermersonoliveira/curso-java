59846f3f32b4e46527e3bb3d14d27cec5821c4cb
ermersonoliveira <ermerson.dev@gmail.com>
2025-12-22 08:37:44 -0300

    feat: intro to Java primitive data types

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,12 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'n';
+        boolean VivoOuMorto = true;
+        Long SaldoBancario = 999999L;
+    }
+}
+
